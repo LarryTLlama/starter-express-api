f2ccc827651cbed82f6d5283916fe1aeac522f84
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2022-09-03 20:53:03 +0100

    Added server stuff for API

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
-app.all('/', (req, res) => {
+app.get('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.sendFile(__dirname, '/index.html')
+})
+app.get('/api/studios', (req, res) => {
+    axios.get('https://api.themeparks.wiki/preview/parks/UniversalStudiosFlorida/waittime')
+    .then((response) => {
+        res.json(response.data);
+        console.log(response.data);
+    })
+    .catch((err) => {
+        console.error(err);
+    })
 })
 app.listen(process.env.PORT || 3000)
