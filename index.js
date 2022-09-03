a41de50ce91d9189d601977c1b0bccc53877f605
LarryTLlama <79579625+LarryTLlama@users.noreply.github.com>
2022-09-03 20:58:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const axios = require('axios')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.sendFile(__dirname, '/index.html')
-})
+app.get('/',function(req,res) {
+    res.sendFile('index.html');
+  });
 app.get('/api/studios', (req, res) => {
     axios.get('https://api.themeparks.wiki/preview/parks/UniversalStudiosFlorida/waittime')
     .then((response) => {
